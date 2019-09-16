3a093a648cd08a477c9b9e5697a98c0497bda5cb
Suresh Patoju <suresh.patoju@in.ibm.com>
2019-09-16 07:05:51 +0000

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+add(x,y):
+        ***Add Function******
+        return x + y
+        def subtract(x,y):
+            ***Subtract Function****
+            return x - y
+            
+            def multiply(x,y):
+                *****Multiply Function*****
+                return x * y
+
+                def divide(x,y):
+                    *** divide Function****
+                    return x/y
